430242856351f2df373351849c00871e41e30b58
alex <format37@gmail.com>
2023-04-06 12:12:47 +0400

    modified:   docker-compose.yml 	modified:   file_server/server.py

--- file_server/server.py
+++ file_server/server.py
@@ -60,6 +60,6 @@ if __name__ == "__main__":
     app.run(
 		host='0.0.0.0',
 		debug=False,
-		port=int(os.environ.get("PORT", 8443)),
+		port=int(os.environ.get("PORT", 443)),
 		ssl_context=context
 		)
