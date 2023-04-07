b53ce434263303eac7f1efd5a22ba488c9a00932
alex <format37@gmail.com>
2023-04-07 10:37:32 +0400

    modified:   data/templates/index.html 	modified:   file_server/server.py

--- file_server/server.py
+++ file_server/server.py
@@ -73,6 +73,116 @@ def serve_legal():
 def serve_index():
 	return render_template('index.html')
 
+@app.route("/players", methods=["GET"])
+def get_players():
+    query = request.args.get("query")
+    res = requests.get(
+        f"{HOST_URL}/api/v1/players?search={query}&page=0&per_page=100")
+    body = res.json()
+    return jsonify(body)
+
+
+@app.route("/teams", methods=["GET"])
+def get_teams():
+    res = requests.get(
+        f"{HOST_URL}/api/v1/teams?page=0&per_page=100")
+    body = res.json()
+    return jsonify(body)
+
+
+@app.route("/games", methods=["GET"])
+def get_games():
+    query_params = [("page", "0")]
+    limit = request.args.get("limit")
+    query_params.append(("per_page", limit or "100"))
+    start_date = request.args.get("start_date")
+    if start_date:
+        query_params.append(("start_date", start_date))
+    end_date = request.args.get("end_date")
+
+    if end_date:
+        query_params.append(("end_date", end_date))
+    seasons = request.args.getlist("seasons")
+
+    for season in seasons:
+        query_params.append(("seasons[]", str(season)))
+    team_ids = request.args.getlist("team_ids")
+
+    for team_id in team_ids:
+        query_params.append(("team_ids[]", str(team_id)))
+
+    res = requests.get(
+        f"{HOST_URL}/api/v1/games?{urllib.parse.urlencode(query_params)}")
+    body = res.json()
+    return jsonify(body)
+
+
+@app.route("/stats", methods=["GET"])
+def get_stats():
+    query_params = [("page", "0")]
+    limit = request.args.get("limit")
+    query_params.append(("per_page", limit or "100"))
+    start_date = request.args.get("start_date")
+    if start_date:
+        query_params.append(("start_date", start_date))
+    end_date = request.args.get("end_date")
+
+    if end_date:
+        query_params.append(("end_date", end_date))
+    player_ids = request.args.getlist("player_ids")
+
+    for player_id in player_ids:
+        query_params.append(("player_ids[]", str(player_id)))
+    game_ids = request.args.getlist("game_ids")
+
+    for game_id in game_ids:
+        query_params.append(("game_ids[]", str(game_id)))
+    res = requests.get(
+        f"{HOST_URL}/api/v1/stats?{urllib.parse.urlencode(query_params)}")
+    body = res.json()
+    return jsonify(body)
+
+
+@app.route("/season_averages", methods=["GET"])
+def get_season_averages():
+    query_params = []
+    season = request.args.get("season")
+    if season:
+        query_params.append(("season", str(season)))
+    player_ids = request.args.getlist("player_ids")
+
+    for player_id in player_ids:
+        query_params.append(("player_ids[]", str(player_id)))
+    res = requests.get(
+        f"{HOST_URL}/api/v1/season_averages?{urllib.parse.urlencode(query_params)}")
+    body = res.json()
+    return jsonify(body)
+
+
+@app.route("/logo.png", methods=["GET"])
+def plugin_logo():
+    filename = 'logo.png'
+    return send_file(filename, mimetype='image/png')
+
+@app.route("/.well-known/ai-plugin.json", methods=["GET"])
+def plugin_manifest():
+    host = request.headers['Host']
+    with open("ai-plugin.json") as f:
+        text = f.read()
+        # This is a trick we do to populate the PLUGIN_HOSTNAME constant in the manifest
+        text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+        return Response(text, mimetype="text/json")
+
+
+@app.route("/openapi.yaml", methods=["GET"])
+def openapi_spec():
+    host = request.headers['Host']
+    with open("openapi.yaml") as f:
+        text = f.read()
+        # This is a trick we do to populate the PLUGIN_HOSTNAME constant in the OpenAPI spec
+        text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+        return Response(text, mimetype="text/yaml")
+
 
 if __name__ == "__main__":
     app.run(
